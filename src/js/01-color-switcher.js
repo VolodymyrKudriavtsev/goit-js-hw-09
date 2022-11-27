51ed27ee3158ef6cbf1fa78fe527c437bafac424
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-11-27 11:02:17 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let intervalId = null;
 
+const onStartBtnClick = e => {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onStoptBtnClick = e => {
+  clearInterval(intervalId);
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStoptBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
