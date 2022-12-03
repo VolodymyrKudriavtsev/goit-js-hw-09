55deac35f8707a912bf8224798e3f2f316551a3a
VolodymyrKudriavtsev <108152577+VolodymyrKudriavtsev@users.noreply.github.com>
2022-12-03 21:35:21 +0000

    Deploying to gh-pages from @ VolodymyrKudriavtsev/goit-js-hw-09@a9bb47f83738e34a1321760ba01be93a12b89698 🚀

--- 03-promises.38e848ba.js
+++
@@ -1,4 +0,0 @@
-(function(o,n){const e=Math.random()>.3;return new Promise(((i,t)=>{setTimeout((()=>{e&&i({position:o,delay:n}),t({position:o,delay:n})}),n)}))})(2,1500).then((function({position:o,delay:n}){console.log(`✅ Fulfilled promise ${o} in ${n}ms`)})).catch((function({position:o,delay:n}){console.log(`❌ Rejected promise ${o} in ${n}ms`)}
-//! position, delay
-));
-//# sourceMappingURL=03-promises.38e848ba.js.map
---
+++ 03-promises.4cdd893d.js
@@ -0,0 +1,2 @@
+function o({position:o,delay:e}){console.log(`✅ Fulfilled promise ${o} in ${e}ms`)}function e({position:o,delay:e}){console.log(`❌ Rejected promise ${o} in ${e}ms`)}let n=2e3,t=0,l=0;console.log("START  INTERVAL !!! - "+Date.now());const i=setInterval((()=>{t+=1,l+=1,n+=1e3,function(o,e){const n=Math.random()>.3;return new Promise(((t,l)=>{setTimeout((()=>{n?(t({position:o,delay:e}),console.log(Date.now())):(l({position:o,delay:e}),console.log(Date.now()))}),0)}))}(l,n).then(o).catch(e),3!==t||clearInterval(i)}),1e3);
+//# sourceMappingURL=03-promises.4cdd893d.js.map
